50642bf45f9c5b2fcd6860db87d8fd19fe7aed54
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-07-12 11:22:20 +0300

    Add store

--- src/app/store.js
+++ src/app/store.js
@@ -1,2 +1,12 @@
-import { createApi } from '@reduxjs/toolkit/query'
+import { configureStore } from "@reduxjs/toolkit";
 
+import { stockApi } from "../services/stockApi";
+
+export default configureStore({
+    reducer: {
+      [stockApi.reducerPath]: stockApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware()
+        .concat(stockApi.middleware)
+  });
